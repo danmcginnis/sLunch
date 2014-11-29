f055e65579d1219b16d39cae2378570fdb4786ae
Dan M McGinnis <mcginnis.dan@gmail.com>
2014-11-29 13:03:18 -0500

    add a template html page and 404 page

--- app/views.py
+++ app/views.py
@@ -4,8 +4,20 @@ from app import app
 
 bootstrap = Bootstrap(app)
 
+title = 'sLunch Calculator'
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html',
-            title='sLunch Calculator')
+            title=title)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html',
+            title=title), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
