d6a46f0b66fe20aaa7d45f716f24fd8ec1c35d73
Dan M McGinnis <mcginnis.dan@gmail.com>
2014-12-02 08:30:28 -0500

    Calculates total per person, removed an unused import

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import Flask,  render_template
 from flask.ext.bootstrap import Bootstrap
 from app import app
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, DecimalField
+from wtforms import SubmitField, DecimalField
 from wtforms.validators import Required
 
 #app = Flask(__name__)
@@ -11,17 +11,25 @@ bootstrap = Bootstrap(app)
 
 title = 'sLunch Calculator'
 
-@app.route('/dan')
-def dan():
-    return "hello"
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    totalBill = 0
+    totalPeople = 0
+    perPerson = 0
+    form = BillForm()
+    if form.validate_on_submit():
+        totalBill = form.TotalBill.data
+        form.TotalBill.data = ''
+        totalPeople = form.TotalPeople.data
+        form.TotalPeople.data = ''
+        perPerson = totalBill / totalPeople
     return render_template('index.html',
             title=title,
-            form=form)
+            form=form,
+            totalBill = totalBill,
+            totalPeople = totalPeople,
+            perPerson = perPerson)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -34,7 +42,7 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-class NameForm(Form):
+class BillForm(Form):
     TotalBill = DecimalField('What is the total bill?', validators=[Required()])
     TotalPeople = DecimalField('How many people are there?', validators=[Required()]) 
 
